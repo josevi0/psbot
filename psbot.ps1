8d48d7de9801d39ce7dbd2a775c0bfa57471044d
josevi0 <34846208+josevi0@users.noreply.github.com>
2019-02-02 22:47:11 +0100

    Update psbot.ps1

--- psbot.ps1
+++ psbot.ps1
@@ -1,5 +1,5 @@
-$ip="github.com/josevi0/psbot/blob/master/"
+$ip="raw.githubusercontent.com/josevi0/psbot/master/"
 $file="cuenta1"
 
-$uri = "http://";$uri+=$ip;$uri+="/";$uri+=$file;$uri+=".txt"
+$uri = "https://";$uri+=$ip;$uri+="/";$uri+=$file;$uri+=".txt"
 $uri
